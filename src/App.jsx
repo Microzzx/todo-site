90fa16cc26d2449c5a95f626e0d34ff2121fa331
Microzzx <janekitpk@gmail.com>
2023-09-22 19:53:26 +0700

    add edit delete icons

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,6 @@ function App() {
   });
   const [todoTitle, setTodoTitle] = useState("");
 
-  console.log(todo);
-
   useEffect(() => {
     localStorage.setItem("TODO_DATA", JSON.stringify(todo));
   }, [todo]);
@@ -27,13 +25,13 @@ function App() {
   };
 
   return (
-    <div className="flex flex-col h-screen overflow-hidden  justify-center items-center bg-green-300 relative">
-      <div className="flex flex-col absolute top-[10%] w-full justify-center items-center space-y-6 p-10 bg-gradient-to-r from-[#6366F1] to-[#A855F7]">
+    <div className="flex flex-col h-screen overflow-hidden  justify-center items-center bg-[#0F172A] relative">
+      <div className="flex flex-col absolute top-[8%] w-full justify-center items-center space-y-6 p-10 bg-gradient-to-r from-[#6366F1] to-[#A855F7]">
         <h1 className="text-black text-4xl font-bold">TODO LIST</h1>
         <form onSubmit={handleSubmit}>
           <input
             type="text"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5"
+            className="bg-white text-gray-900 text-sm rounded-lg p-3 w-[500px]"
             placeholder="Add Your Tasks"
             value={todoTitle}
             onChange={(e) => setTodoTitle(e.target.value)}
@@ -41,7 +39,7 @@ function App() {
           />
         </form>
       </div>
-      <div className="flex flex-wrap justify-center items-center absolute top-[35%] h-[550px] gap-5 overflow-y-scroll hide-scrollbar">
+      <div className="flex flex-wrap justify-center items-center absolute top-[35%] h-[550px] gap-5 overflow-y-scroll ">
         {todo?.map((list, i) => {
           return (
             <Card
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-
+import { BsPencilFill, BsFillTrash3Fill } from "react-icons/bs";
 const Card = ({ index, name, success }) => {
   return (
-    <div className="flex w-[500px] p-4 bg-white rounded-lg animate-slideleft">
-      <div className="h-56">
-        <h1>
-          {index}. {name}
-        </h1>
+    <div className="flex w-[400px] p-10 bg-[#1E293B] hover:bg-[#475366] rounded-lg animate-slideleft relative">
+      <div className="flex w-[80%] absolute gap-6 justify-end ">
+        <BsPencilFill color="white" size={20} className="edit-icon" />
+        <BsFillTrash3Fill color="white" size={20} className="delete-icon" />
+      </div>
+      <div className="flex w-full h-44 justify-center items-center">
+        <h1 className="text-2xl text-white font-bold  truncate">{name}</h1>
       </div>
     </div>
   );
